d8eb06271a69666a4b605dffcc017acca54cff63
Tsegu Abera <abishay2020@gmail.com>
2021-01-14 19:02:32 -0500

    fourth commit

---
+++ rest111/src/rest111/good.java
@@ -0,0 +1,15 @@
+package rest111;
+
+public class good {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	      
+			int a = 10;
+			int b = 21;
+			int c = a +b;
+			System.out.println("the sum of a and b: " + c);
+			
+		}
+
+	}
